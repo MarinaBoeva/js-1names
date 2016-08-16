e509838b571df70f9578558f87402746eec050a9
Marina Boeva <camado6pota@yandex.ru>
2016-08-16 17:24:52 +0300

    added script

---
+++ script.js
@@ -0,0 +1,28 @@
+var names=[];
+var enteredName;
+writeInArr();
+enteredName=prompt('Для входа введите свое имя:','');
+findInArr();
+showMessage();
+function writeInArr() {
+for (var i = 0; i<5; i++) {
+  names[i]=prompt('Введите имя:','');
+}
+}
+function findInArr() {
+   for (var i = 0; i < names.length; i++) {
+    names[i];
+    if(names[i] === enteredName){
+    marker=true;//глобальная переменная
+    break;
+  }
+ }
+}
+function showMessage() {
+  if (window.marker === true){
+  alert(enteredName+',вы успешно вошли!');
+}
+   else {
+   alert('Неверно введено имя пользователя!!!');
+ }
+  }
